a4433258c020f99d770ff3ceccc386ccba2698bc
Niharika209 <6308niharika@gmail.com>
2025-12-08 18:14:09 +0530

    update

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,9 +1,6 @@
-import project1 from "../assets/projects/project-1.jpg";
-import project2 from "../assets/projects/project-2.jpg";
-import project3 from "../assets/projects/project-3.jpg";
-import project4 from "../assets/projects/project-4.jpg";
+export const HERO_CONTENT = `I am a 4th-year B.Tech CSE student, a passionate Full Stack Web Developer and AI enthusiast, currently pursuing a B.Tech in Computer Science at JK Lakshmipat University. I enjoy building user-focused, scalable web applications using the MERN stack, JavaScript, and PostgreSQL, and have hands-on experience with cloud storage (Amazon S3, Filebase) and authentication (JWT, Firebase). I use tools like GitHub Copilot and ChatGPT to boost efficiency and support development. I’ve also explored deep learning and computer vision by building image segmentation projects using U-Net and PyTorch.
 
-export const HERO_CONTENT = `I am a 3rd-year B.Tech CSE student with a passion for research, data analytics, and web development. Throughout my college journey, I have worked on multiple projects across domains such as data analysis, databases, automation, and machine learning. Also, I have developed a foundation in web development by learning HTML, CSS, and JavaScript. I am currently expanding my skills by learning React. I look forward to further exploring these domains and applying my skills to create impactful solutions.`;
+Other than this I am passionate about dance, travel, journaling, swimming and reading as my hobbies. Let's connect to get to know more about one another..`;
 
 export const ABOUT_TEXT = `I am Niharika Sharma, a 3rd-year B.Tech Computer Science student at JK Lakshmipat University, where I’m shaping my skills in research, machine learning, and data analytics. My academic journey has been a blend of hands-on projects and a strong drive to learn. I’ve worked on diverse projects like predicting SDG goals and analyzing air quality, each expanding my understanding of data’s impact on real-world problems.
 
